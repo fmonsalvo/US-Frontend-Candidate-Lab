1368b9e17a5672202928010b0f3cfc85adc8d93d
eabait <estebanabait@gmail.com>
2013-08-16 01:50:24 -0300

    Creation of RSVP message element to provide feedback. Added JavaScript handlers for invitation buttons

--- www/js/main.js
+++ www/js/main.js
@@ -1,1 +1,35 @@
+$(function() {
 
+  function onClick(e) {
+    var button = $(e.currentTarget);
+    var accepted = button.hasClass('js-accept');
+    var message = accepted ?
+      'You have accepted this invitation' :
+      'You have rejected this invitation';
+
+    $('.rspv-message').hide();
+
+    $('.js-accept')
+      .attr('disabled', accepted)
+      .toggleClass('selected', accepted)
+      .toggleClass('loading', accepted);
+
+    $('.js-reject')
+      .attr('disabled', !accepted)
+      .toggleClass('selected', !accepted)
+      .toggleClass('loading', !accepted);
+
+    setTimeout(function() {
+      $('.rspv-message')
+      .text(message)
+      .removeClass('hidden')
+      .show();
+
+      $('.invitation-button').removeClass('loading');
+    }, 2000);
+  }
+
+  $('.js-accept').click(onClick);
+  $('.js-reject').click(onClick);
+
+});
